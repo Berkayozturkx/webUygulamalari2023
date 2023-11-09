942666c4cf980f68b55c06028ce20c4ae7eec51f
Berkay Ozturk <berkay_ozturk0151@hotmail.com>
2023-11-09 15:17:48 +0300

    dördüncü hafta

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const connection = require("./db/mysql_connect")
+const router=require("./routers")
+require('dotenv/config')
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
+app.use("/api", router)
 app.listen(process.env.PORT)
---
+++ routers/routers.js
@@ -0,0 +1,8 @@
+const router=require('express').Router()
+
+router.post("/login")
+
+router.post("/register")
+
+// post: veri gönderme get: veri getirme put: veri güncelleme patch: veri güncelleme delete: veri silme
+module.exports=router
